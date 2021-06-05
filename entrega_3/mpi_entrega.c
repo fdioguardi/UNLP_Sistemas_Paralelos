6b7c78859f78e7d279bba53db9d751bff887f8f4
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-06-05 10:28:04 -0300

    refactor: move secuencial algorithm
    Execute it along with the verification to avoid having the compiler yell
    about variable declarations.

--- entrega_3/mpi_entrega.c
+++ entrega_3/mpi_entrega.c
@@ -46,11 +46,8 @@ int main(int argc, char* argv[]){
 	double *blockR1, *blockR2, *blockM, *blockT, *blockRA, *blockRB, *blockC;
 	double average[2], localAverage[2];
 	double commTimes[AMOUNT_COMMS], maxCommTimes[AMOUNT_COMMS], minCommTimes[AMOUNT_COMMS], commTime, totalTime;
-	double *A, *B, *C, *C_secuencial, *T, *M, *R1, *R2, *RA, *RB, num, aSin, aCos, timetick_start, timetick_end, *ablk, *bblk, *cblk, average1;
+	double *A, *B, *C, *T, *M, *R1, *R2, *RA, *RB, num, aSin, aCos, timetick_start, timetick_end, *ablk, *bblk, *cblk, average1;
 	int N, i, j, k, bs, offset_i, offset_j, row_index, f, c, h, offset_f, offset_c, mini_row_index, size;
-	double average2, timetick_start_secuencial, timetick_end_secuencial;
-
-
 
 	// Setea cantidad de hilos y rank
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
@@ -84,9 +81,9 @@ int main(int argc, char* argv[]){
 	B  = (double*)malloc(sizeof(double)*size); // ordenada por columnas
 
 	if (rank == COORDINATOR) {
+
 		// Aloca memoria para las matrices en el coordinador
 		C  = (double*)malloc(sizeof(double)*size); // ordenada por filas
-		C_secuencial = (double*)malloc(sizeof(double)*size); // ordenada por filas
 		T  = (double*)malloc(sizeof(double)*size); // ordenada por filas
 		M  = (double*)malloc(sizeof(double)*size); // ordenada por filas
 		R1 = (double*)malloc(sizeof(double)*size); // ordenada por filas
@@ -101,111 +98,9 @@ int main(int argc, char* argv[]){
 			B[i] = randFP(0, 10);
 			T[i] = randFP(0, 10);
 			M[i] = randFP(0, DOUBLE_PI);
-			R1[i] = 0;
-			R2[i] = 0;
 			RA[i] = 0;
 			RB[i] = 0;
 		}
-
-
-		/*********************************** Secuencial ******************************/
-
-		average1 = 0;
-		average2 = 0;
-
-		// Inicia el timer
-		timetick_start_secuencial = dwalltime();
-
-		// Calcula Rs y su promedio
-		for (i = 0; i < size; i++) {
-			num = (1 - T[i]);
-			aSin = sin(M[i]);
-			aCos = cos(M[i]);
-			R1[i] = num * (1 - aCos) + (T[i] * aSin);
-			R2[i] = num * (1 - aSin) + (T[i] * aCos);
-
-			average1 += R1[i];
-			average2 += R2[i];
-		}
-
-		// RA = R1 * A
-		for (i = 0; i < N; i += bs)
-		{
-			offset_i = i * N;
-			for (j = 0; j < N; j += bs)
-			{
-				offset_j = j * N;
-				cblk = &RA[offset_i + j];
-
-				for  (k = 0; k < N; k += bs)
-				{
-					ablk = &R1[offset_i + k];
-					bblk = &A[offset_j + k];
-
-					for (f = 0; f < bs; f++)
-					{
-						offset_f = f * N;
-						for (c = 0; c < bs; c++)
-						{
-							offset_c = c * N;
-							mini_row_index = offset_f + c;
-
-							for  (h = 0; h < bs; h++)
-							{
-								cblk[mini_row_index] += ablk[offset_f + h] * bblk[offset_c + h];
-							}
-						}
-					}
-				}
-			}
-		}
-
-		// RB = R2 * B
-		for (i = 0; i < N; i += bs)
-		{
-			offset_i = i * N;
-			for (j = 0; j < N; j += bs)
-			{
-				offset_j = j * N;
-				cblk = &RB[offset_i + j];
-
-				for  (k = 0; k < N; k += bs)
-				{
-					ablk = &R2[offset_i + k];
-					bblk = &B[offset_j + k];
-
-					for (f = 0; f < bs; f++)
-					{
-						offset_f = f * N;
-						for (c = 0; c < bs; c++)
-						{
-							offset_c = c * N;
-							mini_row_index = offset_f + c;
-
-							for  (h = 0; h < bs; h++)
-							{
-								cblk[mini_row_index] += ablk[offset_f + h] * bblk[offset_c + h];
-							}
-						}
-					}
-				}
-			}
-		}
-
-
-		average1 = (average1 / size) * (average2 / size);
-
-		// Calcula C_secuencial
-		for (i = 0; i < size; i++) {
-			C_secuencial[i] = T[i] + average1 * (RA[i] + RB[i]);
-		}
-
-		// Detiene el timer
-		timetick_end_secuencial = dwalltime();
-
-		printf("Tiempo en segundos del secuencial %f\n", timetick_end_secuencial - timetick_start_secuencial);
-
-		average1 = 0;
 	}
 
 
@@ -328,9 +223,116 @@ int main(int argc, char* argv[]){
 	MPI_Reduce(commTimes, maxCommTimes, AMOUNT_COMMS, MPI_DOUBLE, MPI_MAX, COORDINATOR, MPI_COMM_WORLD);
 
 
-	/*****************************************************************/
+	/*********************************** Secuencial ******************************/
 
 	if (rank == COORDINATOR) {
+
+		double average2, timetick_start_secuencial, *C_secuencial,  timetick_end_secuencial;
+
+		C_secuencial = (double*)malloc(sizeof(double)*size); // ordenada por filas
+
+		for(i = 0; i < size ; i++) {
+			RA[i] = 0;
+			RB[i] = 0;
+		}
+
+		average1 = 0;
+		average2 = 0;
+
+		// Inicia el timer
+		timetick_start_secuencial = dwalltime();
+
+		// Calcula Rs y su promedio
+		for (i = 0; i < size; i++) {
+			num = (1 - T[i]);
+			aSin = sin(M[i]);
+			aCos = cos(M[i]);
+			R1[i] = num * (1 - aCos) + (T[i] * aSin);
+			R2[i] = num * (1 - aSin) + (T[i] * aCos);
+
+			average1 += R1[i];
+			average2 += R2[i];
+		}
+
+		// RA = R1 * A
+		for (i = 0; i < N; i += bs)
+		{
+			offset_i = i * N;
+			for (j = 0; j < N; j += bs)
+			{
+				offset_j = j * N;
+				cblk = &RA[offset_i + j];
+
+				for  (k = 0; k < N; k += bs)
+				{
+					ablk = &R1[offset_i + k];
+					bblk = &A[offset_j + k];
+
+					for (f = 0; f < bs; f++)
+					{
+						offset_f = f * N;
+						for (c = 0; c < bs; c++)
+						{
+							offset_c = c * N;
+							mini_row_index = offset_f + c;
+
+							for  (h = 0; h < bs; h++)
+							{
+								cblk[mini_row_index] += ablk[offset_f + h] * bblk[offset_c + h];
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// RB = R2 * B
+		for (i = 0; i < N; i += bs)
+		{
+			offset_i = i * N;
+			for (j = 0; j < N; j += bs)
+			{
+				offset_j = j * N;
+				cblk = &RB[offset_i + j];
+
+				for  (k = 0; k < N; k += bs)
+				{
+					ablk = &R2[offset_i + k];
+					bblk = &B[offset_j + k];
+
+					for (f = 0; f < bs; f++)
+					{
+						offset_f = f * N;
+						for (c = 0; c < bs; c++)
+						{
+							offset_c = c * N;
+							mini_row_index = offset_f + c;
+
+							for  (h = 0; h < bs; h++)
+							{
+								cblk[mini_row_index] += ablk[offset_f + h] * bblk[offset_c + h];
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+		average1 = (average1 / size) * (average2 / size);
+
+		// Calcula C_secuencial
+		for (i = 0; i < size; i++) {
+			C_secuencial[i] = T[i] + average1 * (RA[i] + RB[i]);
+		}
+
+		// Detiene el timer
+		timetick_end_secuencial = dwalltime();
+
+		printf("Tiempo en segundos del secuencial %f\n", timetick_end_secuencial - timetick_start_secuencial);
+
+
+
 		/* printf("Tiempo en segundos con MPI %f\n", timetick_end - timetick_start); */
 
 		// Comprueba que C y C_secuencial sean iguales
