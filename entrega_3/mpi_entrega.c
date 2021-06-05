cad7dbba529ef07b41d8c82121203e54f5f6194e
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-06-05 15:22:05 -0300

    feat: improves performance

--- entrega_3/mpi_entrega.c
+++ entrega_3/mpi_entrega.c
@@ -1,5 +1,3 @@
-// TODO: calculo del tiempo como la gente, borrar los calculos te tiempo de la entrega anterior
-
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,19 +29,17 @@ double randFP(double min, double max) {
 
 /*****************************************************************/
 
-int COORDINATOR = 0;
-int AMOUNT_COMMS = 8;
-
 // Main del programa
 int main(int argc, char* argv[]){
 
 	// Inicializa MPI
 	MPI_Init(&argc, &argv);
 
+	int COORDINATOR = 0;
+	int AMOUNT_COMMS = 6;
 
 	int numProcs, rank, stripSize, rowAmount, cellAmount, pos;
 	double localAverage1, localAverage2;
-	double *blockR1, *blockR2, *blockM, *blockT, *blockRA, *blockRB, *blockC;
 	double average[2], localAverage[2];
 	double commTimes[AMOUNT_COMMS], maxCommTimes[AMOUNT_COMMS], minCommTimes[AMOUNT_COMMS], commTime, totalTime;
 	double *A, *B, *C, *T, *M, *R1, *R2, *RA, *RB, num, aSin, aCos, timetick_start, timetick_end, *ablk, *bblk, *cblk, average1;
@@ -55,10 +51,10 @@ int main(int argc, char* argv[]){
 
 	// Controla los argumentos al programa
 	if ((argc != 3)
-		|| ((N = atoi(argv[1])) <= 0)
-		|| ((bs = atoi(argv[2])) <= 0)
-		|| (N % bs != 0)
-		|| ((rowAmount = (N / numProcs)) < bs))
+	|| ((N = atoi(argv[1])) <= 0)
+	|| ((bs = atoi(argv[2])) <= 0)
+	|| (N % bs != 0)
+	|| ((rowAmount = (N / numProcs)) < bs))
 	{
 		printf("\nError en los parámetros. Usage: ./%s N BS (N debe ser multiplo de BS y BS debe ser menor que N/número_de_procesos)\n", argv[0]);
 		exit(1);
@@ -67,8 +63,6 @@ int main(int argc, char* argv[]){
 	cellAmount = rowAmount * N;
 	size = N*N;
 
-/* 	printf("soy el proceso: %d\nrowAmount: %d\ncellAmount: %d\nN: %d\nbs: %d\nnumProcs: %d", rank, rowAmount, cellAmount, N, bs, numProcs); */
-/* 	exit(0); */
 
 	// Inicializa el randomizador
 	time_t t;
@@ -92,7 +86,7 @@ int main(int argc, char* argv[]){
 		RB = (double*)malloc(sizeof(double)*size); // ordenada por filas
 
 
-		// Inicializa las matrices A, B, T, M, R1, R2, RA, y RB
+		// Inicializa las matrices A, B, T, M, RA, y RB
 		for(i = 0; i < size ; i++) {
 			A[i] = randFP(0, 10);
 			B[i] = randFP(0, 10);
@@ -102,46 +96,50 @@ int main(int argc, char* argv[]){
 			RB[i] = 0;
 		}
 	}
+	else {
+		// Setear los bloques
+		C  = (double*)malloc(sizeof(double)*cellAmount);
+		T  = (double*)malloc(sizeof(double)*cellAmount);
+		M  = (double*)malloc(sizeof(double)*cellAmount);
+		R1 = (double*)malloc(sizeof(double)*cellAmount);
+		R2 = (double*)malloc(sizeof(double)*cellAmount);
+		RA = (double*)malloc(sizeof(double)*cellAmount);
+		RB = (double*)malloc(sizeof(double)*cellAmount);
+	}
 
 
 	/*********************************** MPI ******************************/
 
 
-	// Setear los bloques
-	blockR1 = (double*)malloc(sizeof(double)*cellAmount);
-	blockR2 = (double*)malloc(sizeof(double)*cellAmount);
-	blockM  = (double*)malloc(sizeof(double)*cellAmount);
-	blockT  = (double*)malloc(sizeof(double)*cellAmount);
-	blockRA = (double*)malloc(sizeof(double)*cellAmount);
-	blockRB = (double*)malloc(sizeof(double)*cellAmount);
-	blockC  = (double*)malloc(sizeof(double)*cellAmount);
-
-	// Cronometrado de la primer comunicación
 	commTimes[0] =  dwalltime();
 
-	MPI_Scatter(M, cellAmount, MPI_DOUBLE, blockM, cellAmount, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
-	MPI_Scatter(T, cellAmount, MPI_DOUBLE, blockT, cellAmount, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
-
-	commTimes[1] = dwalltime();
+	// Reparte las matrices M y T
+	MPI_Scatter(M, cellAmount, MPI_DOUBLE, M, cellAmount, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
+	MPI_Scatter(T, cellAmount, MPI_DOUBLE, T, cellAmount, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
 
 	// Broadcastea las matrices que se usan enteras
-	commTimes[2] =  dwalltime();
-
 	MPI_Bcast(A, size, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
 	MPI_Bcast(B, size, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
 
-	commTimes[3] = dwalltime();
+	commTimes[1] = dwalltime();
 
 	// Calcula Rs
 	for (i = 0; i < cellAmount; i++) {
-		num = (1 - blockT[i]);
-		aSin = sin(blockM[i]);
-		aCos = cos(blockM[i]);
-		blockR1[i] = num * (1 - aCos) + (blockT[i] * aSin);
-		blockR2[i] = num * (1 - aSin) + (blockT[i] * aCos);
-		localAverage[0] += blockR1[i];
-		localAverage[1] += blockR2[i];
+		num = (1 - T[i]);
+		aSin = sin(M[i]);
+		aCos = cos(M[i]);
+		R1[i] = num * (1 - aCos) + (T[i] * aSin);
+		R2[i] = num * (1 - aSin) + (T[i] * aCos);
+		localAverage[0] += R1[i];
+		localAverage[1] += R2[i];
 	}
+
+	// Calcula la suma de Rs
+	commTimes[2] = dwalltime();
+
+	MPI_Allreduce(localAverage, average, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+	commTimes[3] = dwalltime();
 
 	// RA = R1 * A
 	for (i = 0; i < rowAmount; i += bs)
@@ -150,11 +148,11 @@ int main(int argc, char* argv[]){
 		for (j = 0; j < N; j += bs)
 		{
 			offset_j = j * N;
-			cblk = &blockRA[offset_i + j];
+			cblk = &RA[offset_i + j];
 
 			for  (k = 0; k < N; k += bs)
 			{
-				ablk = &blockR1[offset_i + k];
+				ablk = &R1[offset_i + k];
 				bblk = &A[offset_j + k];
 
 				for (f = 0; f < bs; f++)
@@ -177,11 +175,11 @@ int main(int argc, char* argv[]){
 		for (j = 0; j < N; j += bs)
 		{
 			offset_j = j * N;
-			cblk = &blockRB[offset_i + j];
+			cblk = &RB[offset_i + j];
 
 			for  (k = 0; k < N; k += bs)
 			{
-				ablk = &blockR2[offset_i + k];
+				ablk = &R2[offset_i + k];
 				bblk = &B[offset_j + k];
 
 				for (f = 0; f < bs; f++)
@@ -198,25 +196,20 @@ int main(int argc, char* argv[]){
 						} } } } } }
 
 	// Calcula promedios de Rs
-	commTimes[4] = dwalltime();
-
-	MPI_Allreduce(localAverage, average, 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-	commTimes[5] = dwalltime();
-
 	average1 = (average[0] / size) * (average[1] / size);
 
 	// Calcula C
 	for (i = 0; i < cellAmount; i++) {
-		blockC[i] = blockT[i] + average1 * (blockRA[i] + blockRB[i]);
+		C[i] = T[i] + average1 * (RA[i] + RB[i]);
 	}
 
 
-	commTimes[6] = dwalltime();
+	// Junta el resultado
+	commTimes[4] = dwalltime();
 
-	MPI_Gather(blockC, cellAmount, MPI_DOUBLE, C, cellAmount, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
+	MPI_Gather(C, cellAmount, MPI_DOUBLE, C, cellAmount, MPI_DOUBLE, COORDINATOR, MPI_COMM_WORLD);
 
-	commTimes[7] = dwalltime();
+	commTimes[5] = dwalltime();
 
 	// Totaliza los tiempos de comunicación
 	MPI_Reduce(commTimes, minCommTimes, AMOUNT_COMMS, MPI_DOUBLE, MPI_MIN, COORDINATOR, MPI_COMM_WORLD);
@@ -229,8 +222,10 @@ int main(int argc, char* argv[]){
 
 		double average2, timetick_start_secuencial, *C_secuencial,  timetick_end_secuencial;
 
+		// Aloca matriz secuencial
 		C_secuencial = (double*)malloc(sizeof(double)*size); // ordenada por filas
 
+		// Reinicia variables
 		for(i = 0; i < size ; i++) {
 			RA[i] = 0;
 			RB[i] = 0;
@@ -318,7 +313,7 @@ int main(int argc, char* argv[]){
 			}
 		}
 
-
+		// Calcula el promedio de Rs
 		average1 = (average1 / size) * (average2 / size);
 
 		// Calcula C_secuencial
@@ -333,7 +328,6 @@ int main(int argc, char* argv[]){
 
 
 
-		/* printf("Tiempo en segundos con MPI %f\n", timetick_end - timetick_start); */
 
 		// Comprueba que C y C_secuencial sean iguales
 		int check = 1;
@@ -351,6 +345,7 @@ int main(int argc, char* argv[]){
 			totalTime = maxCommTimes[AMOUNT_COMMS - 1] - minCommTimes[0];
 			commTime = 0;
 			for (i = 0; i < AMOUNT_COMMS; i += 2) {
+				printf("tiempo %d - %d = %f\n", i+1, i, maxCommTimes[i + 1] - minCommTimes[i]);
 				commTime += (maxCommTimes[i + 1] - minCommTimes[i]);
 			}
 
@@ -362,30 +357,23 @@ int main(int argc, char* argv[]){
 
 
 
-		// Libera memoria
-		free(C);
+		// Libera memoria secuencial
 		free(C_secuencial);
-		free(T);
-		free(M);
-		free(R1);
-		free(R2);
-		free(RA);
-		free(RB);
 	}
 
 	// Free matrices paralelas
 	free(A);
 	free(B);
-	free(blockC);
-	free(blockM);
-	free(blockT);
-	free(blockR1);
-	free(blockR2);
-	free(blockRA);
-	free(blockRB);
+	free(C);
+	free(M);
+	free(T);
+	free(R1);
+	free(R2);
+	free(RA);
+	free(RB);
 
 	// Termina MPI
 	MPI_Finalize();
 
-	return(0);
+	return EXIT_SUCCESS;
 }
